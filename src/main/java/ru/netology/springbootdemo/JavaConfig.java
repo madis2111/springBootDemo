6255b475f0cad5ad0ef9c70b7e6c442082b915bc
Madis21 <ssmirnoff21@gmail.com>
2024-05-30 23:08:57 +0500

    Initial commit

---
+++ src/main/java/ru/netology/springbootdemo/JavaConfig.java
@@ -0,0 +1,2 @@
+package ru.netology.springbootdemo;public class JavaConfig {
+}
---
+++ src/main/java/ru/netology/springbootdemo/controller/ProfileController.java
@@ -0,0 +1,20 @@
+package ru.netology.springbootdemo;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/")            // localhost:8080/profile
+public class ProfileController {
+    private SystemProfile profile;
+
+    public ProfileController(SystemProfile profile) {
+        this.profile = profile;
+    }
+
+    @GetMapping("profile")
+    public String getProfile() {
+        return profile.getProfile();
+    }
+}
